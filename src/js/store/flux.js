1f66b6b8ecd143a2ec0c27ce784509681c2ac632
samvalvi <samuelvalerin@gmail.com>
2021-04-02 17:02:02 +0000

    bug appContext fixed

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,15 +12,87 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			people: [],
+			planets: [],
+			vehicles: [],
+			favorites: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
+			loadPeople: () => {
+				fetch("https://swapi.dev/api/people/", {
+					method: `GET`,
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						//console.log(resp.ok);
+						if (!resp.ok) throw Error(resp.statusText);
+						//console.log(resp.status);
+						return resp.json();
+					})
+					.then(data => {
+						setStore({ people: data.results });
+					})
+					.catch(error => {
+						console.log(error);
+					});
+				/** https://swapi.dev/api/people/1/
+					fetch("https://swapi.dev/api/people/1/").then().then(data => setStore({ "foo": data.bar }))
+                */
+			},
+			loadPlanets: () => {
+				fetch("https://swapi.dev/api/planets/", {
+					method: `GET`,
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						console.log(resp.ok);
+						if (!resp.ok) throw Error(resp.statusText);
+						console.log(resp.status);
+						return resp.json();
+					})
+					.then(data => {
+						setStore({ planets: data.results });
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			},
+			loadVehicles: () => {
+				fetch("https://swapi.dev/api/vehicles/", {
+					method: `GET`,
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						console.log(resp.ok);
+						if (!resp.ok) throw Error(resp.statusText);
+						console.log(resp.status);
+						return resp.json();
+					})
+					.then(data => {
+						setStore({ vehicles: data.results });
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			},
+			addFavorites: (id, type) => {
+				const store = getStore();
+				const favorite = { id, type };
+				setStore({ favorites: store.favorites.concat(favorite) });
+			},
 			loadSomeData: () => {
-				/**
+				/** 
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
