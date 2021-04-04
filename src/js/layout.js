75f97baa70f69fe1143ee98f0baa8ca79f819391
samvalvi <samuelvalerin@gmail.com>
2021-04-04 17:20:32 +0000

    favorites button bug

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
-import { DetailInfoCharacters } from "./views/character";
-import { DetailInfoPlanet } from "./views/planet";
+import { Characters } from "./views/character";
+import { Planets } from "./views/planet";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
@@ -26,10 +24,10 @@ const Layout = () => {
 							<Home />
 						</Route>
 						<Route exact path="/people/:characterid">
-							<DetailInfoCharacters />
+							<Characters />
 						</Route>
 						<Route exact path="/planet/:planetid">
-							<DetailInfoPlanet />
+							<Planets />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
