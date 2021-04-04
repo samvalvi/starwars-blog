58d12cd4918ee85a08d3820483626cb10e60c012
samvalvi <samuelvalerin@gmail.com>
2021-04-04 18:57:50 +0000

    bug layout fixed

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
 import { DetailInfoCharacters } from "./views/character";
 import { DetailInfoPlanet } from "./views/planet";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
