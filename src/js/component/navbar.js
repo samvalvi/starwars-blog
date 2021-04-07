716473d63d72d976e41317188aeb1d55a43fc311
samvalvi <samuelvalerin@gmail.com>
2021-04-07 22:43:26 +0000

    final commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import starwars from "../../img/logo.png";
+import { NavDropdown, DropdownButton } from "react-bootstrap";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
@@ -13,42 +14,26 @@ export const Navbar = () => {
 					<img src={starwars} width="100" height="50" className="d-inline-block align-top" alt="logo" />
 				</div>
 			</Link>
-
-			<div className="dropdown">
-				<button
-					className="btn btn-warning dropdown-toggle"
-					type="button"
-					id="favoriteList"
-					data-toggle="dropdown"
-					aria-haspopup="true"
-					aria-expanded="false">
-					Favorites
-					<span className="badge bg-secondary m-1">{store.favorites.length}</span>
-				</button>
-
-				{store.favorites.length === 0 ? (
-					<ul className="dropdown-menu" aria-labelledby="favoriteList">
-						<li className="dropdown-item text-center">(empty)</li>
-					</ul>
-				) : (
-					<ul className="dropdown-menu" aria-labelledby="favoriteList">
-						{store.favorites.length === 0 ? (
-							<p className="text-center">(Empty)</p>
-						) : (
-							store.favorites.map((fav, i) => (
-								<li className="dropdown-item" key={i}>
+			<div className="mx-2">
+				<DropdownButton variant="primary" title={"Favorites " + store.favorites.length}>
+					{store.favorites.length == 0 ? (
+						<NavDropdown.Item href="#action/3.1">(Empty)</NavDropdown.Item>
+					) : (
+						store.favorites.map((fav, i) => {
+							return (
+								<NavDropdown.Item key={i}>
 									{fav}
 									<i
-										className="fas fa-trash"
+										className="fas fa-trash float-right"
 										onClick={() => {
 											actions.deleteFavorite(fav);
 										}}
 									/>
-								</li>
-							))
-						)}
-					</ul>
-				)}
+								</NavDropdown.Item>
+							);
+						})
+					)}
+				</DropdownButton>
 			</div>
 		</nav>
 	);
